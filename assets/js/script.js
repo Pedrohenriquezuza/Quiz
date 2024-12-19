5a02aff153bb6800136ddda2906dbe98ad74ea88
Pedro Henrique Zuza <156633255+Pedrohenriquezuza@users.noreply.github.com>
2024-12-19 10:49:37 -0300

    interação e porcentagem
    Adicionei interações no Quiz e as porcentagens de acertos finais.

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+// Initial Data
+let  currentQuestion = 0
+let correctAnswers = 0
+showQuestion()
+
+// Functions
+function showQuestion(){
+    if(questions[currentQuestion]){
+        let q = questions[currentQuestion]
+        
+        let pct = Math.floor((currentQuestion /  questions.length) * 100)
+
+        document.querySelector('.progress--bar').style.width =  `${pct}%`
+
+        document.querySelector('.scoreArea').style.display = 'none'
+        document.querySelector('.questionArea').style.display = 'block'
+
+        document.querySelector('.question').innerHTML = q.question
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+
+        for(let i in q.options){
+            optionsHtml += `<div data-op="${i}"class="option"><span>${Number(i)+1}</span>${q.options[i]}</div>`;
+        }
+
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach((item) =>{
+            item.addEventListener("click", optionClickEvent)
+        })
+
+    } else {
+        finishQuiz();
+    }
+}
+
+
+function optionClickEvent(e){
+    let clickedOption = Number(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickedOption){
+        correctAnswers++;
+    }
+
+    currentQuestion++;
+    showQuestion()
+}
+
+function finishQuiz(){
+    let points = Math.floor((correctAnswers / questions.length) * 100)
+
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML = `Dá pra melhorar!`
+        document.querySelector('.scorePct').style.color = '#F00'
+    } else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = `Muito Bom!`
+        document.querySelector('.scorePct').style.color = '#FFFF00'
+    } else if (points >= 70 && points < 100){
+        document.querySelector('.scoreText1').innerHTML = `Parabéns!`
+        document.querySelector('.scorePct').style.color = '#0D630D'
+    } else {
+        document.querySelector('.scoreText1').innerHTML = `Wow, Perfeito!`
+        document.querySelector('.scorePct').style.color = '#0D630D'
+    }
+
+    // Porcentagem Final de acerto
+    document.querySelector(".scorePct").innerHTML = `Acertou ${points}%`
+
+    // Frase Final de acerto
+    document.querySelector(".scoreText2").innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswers}`
+
+    document.querySelector('.progress--bar').style.width =  '100%'
+    document.querySelector('.scoreArea').style.display = 'block'
+    document.querySelector('.questionArea').style.display = 'none'
+}
